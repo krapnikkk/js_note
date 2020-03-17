e2132db4d19f77ac087d9759d801717f5108f640
krapnik <krapnik@qq.com>
2020-03-18 00:53:58 +0800

    add commomjs

---
+++ require() 源码解读/2.Module 构造函数/module.js
@@ -0,0 +1,12 @@
+function Module(id, parent) {
+    this.id = id;
+    this.exports = {};
+    this.parent = parent;
+    this.filename = null;
+    this.loaded = false;
+    this.children = [];
+  }
+  
+  module.exports = Module;
+  
+  var module = new Module(filename, parent);
