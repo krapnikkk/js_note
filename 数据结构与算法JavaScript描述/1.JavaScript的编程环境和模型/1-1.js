a49b0ac5030aced2af12f5010ea0cd3d8d2d012f
krapnik <krapnik@qq.com>
2020-04-08 00:30:45 +0800

    back up

---
+++ 数据结构与算法JavaScript描述/1.JavaScript的编程环境和模型/1-1.js
@@ -0,0 +1,12 @@
+/**
+ * JavaScript中的算术运算和数学函数
+ */
+
+var x = 3, y = 1.1;
+console.log(x + y);
+console.log(x * y);
+console.log((x + y) * (x - y));
+
+let z = 9;
+console.log(Math.sqrt(z));
+console.log(Math.abs(y / x));
