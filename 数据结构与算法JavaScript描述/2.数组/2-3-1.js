f4cf789f32de579855a4f610be4bb550baa43127
krapnik <krapnik@qq.com>
2020-04-09 23:53:31 +0800

    归档备份

---
+++ 数据结构与算法JavaScript描述/2.数组/2-3-1.js
@@ -0,0 +1,11 @@
+var names = ["Mike", "David", "Cynthia", "George", "Jennifer", "Mike", "Clayton"];
+var position = names.indexOf("David");
+if (position >= 0) {
+    console.log(`David at position ${position}`);
+} else {
+    console.log("name not found");
+}
+var firstPos = names.indexOf("Mike"), lastPos = names.lastIndexOf("Mike");
+console.log(`firstFound:${firstPos},lastFound:${lastPos}`);
+
+
