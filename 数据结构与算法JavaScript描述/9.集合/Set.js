ab59acf7afa09484669b123f80e1357be2fb106f
krapnik <krapnik@qq.com>
2020-05-07 00:15:11 +0800

    归档备份

--- 数据结构与算法JavaScript描述/9.集合/Set.js
+++ 数据结构与算法JavaScript描述/9.集合/Set.js
@@ -31,34 +31,79 @@ class Set {
     }
 
     contains(item) {
-        return this.dataStore.indexOf(item)>-1;
+        return this.dataStore.indexOf(item) > -1;
     }
 
-    union(set){
+    union(set) {
         var tempSet = new Set();
-        for (var i = 0; i <this.dataStore.length; i++){
+        for (var i = 0; i < this.dataStore.length; i++) {
             tempSet.add(this.dataStore[i]);
         }
 
-        for(var i = 0; i <set.dataStore.length; i++){
-            if(!tempSet.contains(set.dataStore[i])){
+        for (var i = 0; i < set.dataStore.length; i++) {
+            if (!tempSet.contains(set.dataStore[i])) {
                 tempSet.push(set.dataStore[i]);
             }
         }
         return tempSet;
     }
 
-    intersect(set){
+    intersect(set) {
         var tempSet = new Set();
-        for (var i = 0; i <this.dataStore.length;i++){
+        for (var i = 0; i < this.dataStore.length; i++) {
             if (set.contains(this.dataStore[i])) {
-               tempSet.add(this.dataStore[i]); 
+                tempSet.add(this.dataStore[i]);
             }
         }
         return tempSet;
     }
 
-    
+    subset(set) {
+        if (this.size() > set.size()) {
+            return false;
+        } else {
+            for (var member in this.dataStore) {
+                if (!set.contains(member)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    difference(set) {
+        var tempSet = new Set();
+        for (var i = 0; i < this.dataStore.length; i++) {
+            if (!set.contains(this.dataStore[i])) {
+                tempSet.add(this.dataStore[i]);
+            }
+        }
+        return tempSet;
+    }
+
+    sort() {
+        this.dataStore = this.dataStore.sort();
+    }
+
+    higher(item) {
+        var tempArr = [];
+        for (var i = 0; i < this.dataStore.length; i++) {
+            if (this.dataStore[i] > item) {
+                tempArr.push(this.dataStore[i]);
+            }
+        }
+        return tempArr.sort()[0];
+    }
+
+    lower(item) {
+        var tempArr = [];
+        for (var i = 0; i < this.dataStore.length; i++) {
+            if (this.dataStore[i] < item) {
+                tempArr.push(this.dataStore[i]);
+            }
+        }
+        return tempArr.sort().reverse()[0];
+    }
 }
 
 function test() {
@@ -70,14 +115,14 @@ function test() {
     names.add("Raymond");
     if (names.add("Mike")) {
         console.log("Mike added")
-    }else {
+    } else {
         console.log("Can't add Mike, must already be in set");
     }
     console.log(names.show());
     var removed = "Mike";
     if (names.remove(removed)) {
         console.log(removed + " removed.");
-    }else {
+    } else {
         console.log(removed + " not removed.");
     }
     names.add("Clayton");
@@ -85,7 +130,7 @@ function test() {
     removed = "Alisa";
     if (names.remove("Alisa")) {
         console.log(removed + " removed.");
-    }else {
+    } else {
         console.log(removed + " not removed.");
     }
 }
